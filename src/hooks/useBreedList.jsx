de05b9803b4daf23c3c8d93c500ccbd4cdc344cf
Johnny Estrada <johnnye562@yahoo.com>
2023-09-21 19:34:21 -0700

    Create useBreedList component

---
+++ src/hooks/useBreedList.jsx
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react";
+
+const localCache = {};
+
+export default function useBreedList(animal) {
+  const [breedList, setBreedList] = useState([]);
+  // if you have a custom hook that is doing something
+  // that you have to wait on (e.g data fetching)
+  // having status makes is easy to test the component
+  const [status, setStatus] = useState("unloaded");
+
+  useEffect(() => {
+    if (!animal) {
+      setBreedList([]);
+    } else if (localCache[animal]) {
+      setBreedList(localCache[animal]);
+    } else {
+      requestBreedList();
+    }
+
+    async function requestBreedList() {
+      setBreedList([]);
+      setStatus("loading");
+      const res = await fetch(
+        `http://pets-v2.dev-apis.com/breeds?animal=${animal}`
+      );
+      const json = await res.json();
+      localCache[animal] = json.breeds || [];
+      setBreedList(localCache[animal]);
+      setStatus("loaded");
+    }
+  }, [animal]);
+
+  return [breedList, status];
+}
