e0250552108dd5444b3741411e93b013bbaacb98
Johnny Estrada <johnnye562@yahoo.com>
2023-09-22 10:14:23 -0700

    Add routes to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Details from "./components/Details"
 import SearchParams from "./components/SearchParams";
 
 
 const App = () => {
-  return (<div>
-    <h1>Adopt Me!</h1>
-    <SearchParams />
-  </div>
+  return (
+    <BrowserRouter>
+      <header>
+        <Link to="/">Adopt Me!</Link>
+      </header>
+      <Routes>
+        <Route path="/details/:id" element={<Details />} />
+        <Route path="/" element={<SearchParams />} />
+      </Routes>
+    </BrowserRouter>
   )
 };
 
